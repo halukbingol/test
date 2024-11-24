49a0b760a70c6ca41e5b79ed8634d6e37a49b4e6
Haluk Bingol <haluk.bu@gmail.com>
2024-11-24 16:13:23 +0300

    init

---
+++ ZTastA/src/Aa.java
@@ -0,0 +1,8 @@
+
+public class Aa {
+
+	public static void main(String[] args) {
+		System.out.println("Aa");
+	}
+
+}
---
+++ ZTestB/src/Bb.java
@@ -0,0 +1,8 @@
+
+public class Bb {
+
+	public static void main(String[] args) {
+		System.out.println("Bb");
+	}
+
+}
